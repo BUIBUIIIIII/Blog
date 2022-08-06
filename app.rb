8858912990db0058416f9f11a7e532d4d6ecc376
BUIBUIIIIII <ec2-user@ip-10-1-13-143.ap-northeast-1.compute.internal>
2022-08-06 11:13:10 +0000

    Asynchronous processing

--- app.rb
+++ app.rb
@@ -42,50 +42,20 @@ post '/post-articles' do
 end
 
 post '/post-images-file' do
-    # puts params[:image]
     @img_url = ''
     if params[:image]
         img = params[:image]
         tempfile = img[:tempfile]
         upload = Cloudinary::Uploader.upload(tempfile.path)
-        $img_url = upload['url']
+        img_url = upload['url']
+        data = {
+            "success" => 1,
+            "file" => {
+                "url" => img_url
+            }
+        }
+        json = data.to_json
+        puts json
+        return json
     end
-    # @@foo = "bar"
-    # puts $img_url
-    # response = JSON.dump(:"success" => "1", :"file" => {:"url" => $image_url})
-    # json response
-    # puts CLOUDINARY_API_SECRET
-    # redirect '/'
-    # "Hello World"
-    # puts params
 end
-
-get '/get-images-file' do
-    # sleep(10)
-    puts $img_url
-    # json = JSON.parse[res.body]
-    # res = NET::HTTP.get_response
-    data = {
-        :"url" => $img_url
-    }
-    json = data.to_json
-    puts json
-    return json
-    # render :json => result
-    # render json: response
-    # json response
-    # puts json
-    # json response
-    # puts "urllist" + $img_url
-    # @url = $img_url
-    # result = $img_url
-    # result response
-    # render text: $img_url
-end
-
-# post '/post-images-url' do
-#     puts params
-# end
-
-# get '/get-image-url' do
-# end
