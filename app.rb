bfad79a96cb864dd4f975271aa946b79c659ba88
BUIBUIIIIII <ec2-user@ip-10-1-13-143.ap-northeast-1.compute.internal>
2022-10-17 10:13:54 +0000

    comment out

--- app.rb
+++ app.rb
@@ -20,47 +20,47 @@ before do
     end
 end
 
-helpers do
-    def current_user
-        User.find_by(id:session[:user])
-    end
-end
+# helpers do
+#     def current_user
+#         User.find_by(id:session[:user])
+#     end
+# end
 
-get '/' do
-    redirect '/blog'
-end
+# # get '/' do
+#     redirect '/blog'
+# end
 
-get '/signin' do
-    erb :sign_in
-end
+# get '/signin' do
+#     erb :sign_in
+# end
 
-get 'signup' do
-    erb :sign_up
-end
+# get 'signup' do
+#     erb :sign_up
+# end
 
-post '/signin' do
-    user = User.find_by(username: username)
-    if user then
-        if password == user.password then
-            session[:user] = user.id
-        end
-    end
-    redirect '/'
-end
+# post '/signin' do
+#     user = User.find_by(username: username)
+#     if user then
+#         if password == user.password then
+#             session[:user] = user.id
+#         end
+#     end
+#     redirect '/'
+# end
 
-post '/signup' do
-    @user = User.create(mail:params[:mail], password:params[:password],
-    password_confirmation:params[:password_confirmation])
-    if @user.persisted?
-        session[:user] = @user.id
-    end
-    redirect '/'
-end
+# post '/signup' do
+#     @user = User.create(mail:params[:mail], password:params[:password],
+#     password_confirmation:params[:password_confirmation])
+#     if @user.persisted?
+#         session[:user] = @user.id
+#     end
+#     redirect '/'
+# end
 
-get '/signout' do
-    session[:user] = nil
-    redirect '/'
-end
+# get '/signout' do
+#     session[:user] = nil
+#     redirect '/'
+# end
 
 get '/edit' do
     # protected!
